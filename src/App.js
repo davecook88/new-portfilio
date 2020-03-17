e061180901272e8c7996b42c755b5b6e76366046
Dave Cook <DaveCook@hotmail.co.uk>
2020-03-17 14:32:18 +0700

    menu bar coming along

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
 import { Waypoint } from "react-waypoint";
 import calculateInlineStyle from "./Components/Laptop/calculateLaptopStyle";
+import NavButton from './Components/NavButton/NavButton';
 import Laptop from "./Components/Laptop/Laptop";
 import SocialMediaBar from "./Components/SocialMedia/SocialMedia";
 import ReviewCarousel from './Components/Reviews/ReviewCarousel'
@@ -13,7 +14,7 @@ const smallModifiers = {
   heightModifier: 0.45,
   widthModifier: 0.42,
   leftModifier: 0.1,
-  topModifier: 0.1
+  topModifier: 0.2
 };
 const style = calculateInlineStyle(smallModifiers);
 class App extends React.Component {
@@ -22,7 +23,8 @@ class App extends React.Component {
     this.state = {
       updated: false,
       position: 0,
-      laptopStyle: calculateInlineStyle()
+      laptopStyle: calculateInlineStyle(),
+      menuOpen:'open',
     };
   }
   setPosition(waypoint, {force = false} = {}) {
@@ -30,6 +32,15 @@ class App extends React.Component {
     const newPosition = force? waypoint : position === waypoint ? waypoint + 1: waypoint;
     console.log(position, waypoint, newPosition);
     this.setState({ position: newPosition });
+  }
+  setMenuOpenStatus = (newStatus) => {
+    let status = () => {
+      if (!newStatus){
+       return  this.state.menuOpen !== 'open' ? 'open' : 'closed';
+      }
+      return newStatus;
+    }
+    this.setState({menuOpen:status});
   }
   updateLaptopStyle(modifiers) {
     console.log(modifiers);
@@ -41,6 +52,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <NavButton 
+          position={this.state.position}
+          open={(this.state.menuOpen === 'open')}
+          toggleMenu={this.setMenuOpenStatus}
+        />
         <div className="logo holder">
           <NameLogo position={this.state.position} />
         </div>
--- src/Components/NavButton/NavButton.js
+++ src/Components/NavButton/NavButton.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './navbutton.scss'
 
 export default class NavButton extends Component {
   constructor(props) {
@@ -10,7 +11,19 @@ export default class NavButton extends Component {
   render() {
     return (
       <div className="navbutton">
-        
+        <div className="menu-bar">
+          <ul>
+            {this.props.position !== 0 ? <li>home</li> : ''}
+            <li>web design</li>
+            <li>automation</li>
+            <li>about</li>
+            <li>contact</li>
+          </ul>
+        </div>
+        <div className="nav-line-holder" onClick={this.props.toggleMenu}>
+          <div className={`white nav-line one ${this.props.open ? 'closed-cross' : 'open-arrow'}`}></div>
+          <div className={`white nav-line two ${this.props.open ? 'closed-cross' : 'open-arrow'}`}></div>
+        </div>
       </div>
     )
   }
